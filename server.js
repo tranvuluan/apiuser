d90e579dec46c6ceb6f3bb8a3427cec990d35694
tranvuluan <tranvuluan069@gmail.com>
2021-10-22 18:58:46 +0700

    a

--- server.js
+++ server.js
@@ -47,4 +47,4 @@ app.use('/', router);
 
 
 
-app.listen(PORT, () => console.log(`Sever is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Sever is running on ${path.dirname(__filename)}:${PORT}`));
